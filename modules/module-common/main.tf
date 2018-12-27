47b8b7ea1a8b1d08759cfe7790e72bb8bddba4bf
Andrej Krnac <andrej.krnac@piano.io>
2018-12-27 16:34:16 +0100

    Add module-common module

---
+++ modules/module-common/main.tf
@@ -0,0 +1,16 @@
+# TEST
+resource "aws_security_group" "module-common" {
+  name        = "module-common"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
